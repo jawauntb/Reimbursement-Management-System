4b5dc8e5377bb0e62bcfd1200ca82cfe1e080e3b
Jawaun <jawaun.brown95@gmail.com>
2018-09-28 15:11:56 -0400

    made project structure, configured maven build setting

---
+++ src/main/java/com/projects/reimbursementsystem/App.java
@@ -0,0 +1,13 @@
+package com.projects.reimbursementsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
